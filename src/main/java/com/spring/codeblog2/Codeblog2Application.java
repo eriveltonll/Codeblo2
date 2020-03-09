b90125c8d582fec0660481239c8129fcd1da3aa9
Erivelton Laia <erivelton.laia@engdb.com.br>
2020-03-09 17:20:41 -0300

    Testando novo comando de implementação

--- src/main/java/com/spring/codeblog2/Codeblog2Application.java
+++ src/main/java/com/spring/codeblog2/Codeblog2Application.java
@@ -2,9 +2,10 @@ package com.spring.codeblog2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Codeblog2Application {
+public class Codeblog2Application extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Codeblog2Application.class, args);
